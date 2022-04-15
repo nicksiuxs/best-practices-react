3daffe14d9b77133bbf04daf317deca2a3108348
Nicolás Londoño <nicolas.londono@veevart.com>
2022-04-15 17:56:43 -0500

    Add: window size hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import useWindowSize from './hooks/useWindowSize';
+
 
 export default function App() {
+  const windowSize = useWindowSize();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <p>Current screen width: {windowSize.width}</p>
+      <p>Current screen height: {windowSize.height}</p>
+    </>
   )
 }
